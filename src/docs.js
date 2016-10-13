c084fcb8a00e64146c6d9846221f3e739125c43d
Rod Leviton <rod@rodlevtion.com>
2016-10-14 08:49:22 +1100

    add slinky demo screenshot

--- src/docs.js
+++ src/docs.js
@@ -8,9 +8,8 @@ import './docs.styl';
 // Add any javascript here
 if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
   var args = [
-    '\n %c ✰ Slinky ✰ %c %c https://reactivepixels.github.io/get-slinky/ \n\n',
+    '\n %c ✰ Slinky ✰ %c https://reactivepixels.github.io/get-slinky/ \n\n',
     'background: #D04945; color: #ffffff; padding:5px 0;',
-    'background: #eeeeee; padding:5px 0;',
     'background: #404050; color: #ffffff; padding:5px 0;'
   ];
 
